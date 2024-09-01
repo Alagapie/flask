d18a29fe4ee270211a6af5918dc1bba393f7c678
Alagapie <abdulbasitsuleiman660@gmail.com>
2024-09-01 13:43:42 +0100

    Update entrypoint.sh

--- ai-recruiter-main/backend/entrypoint.sh
+++ ai-recruiter-main/backend/entrypoint.sh
@@ -3,8 +3,14 @@
 echo "Start run entrypoint script..."
 
 echo "Environment: $APP_ENV"
+echo "API_ENTRYPOINT: $API_ENTRYPOINT"
+echo "API_HOST: $API_HOST"
+echo "API_PORT: $API_PORT"
 
 if [ "$APP_ENV" = "local" ]; then
     echo "Run app with gunicorn server..."
     gunicorn --bind 0.0.0.0:5000 $API_ENTRYPOINT --timeout 1200 --workers 4
+else
+    echo "Environment is not set to local. Exiting."
+    exit 1
 fi
